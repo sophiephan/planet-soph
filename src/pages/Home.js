a8152f525647d09f949f800afc4c5384609f2aad
sophie364 <sophiephan364@gmail.com>
2024-01-19 22:32:12 +0000

    Basic routing

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to My Website</h1>
+        <p>This is the homepage content. Feel free to add more content here.</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
